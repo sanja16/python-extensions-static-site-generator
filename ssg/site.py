9686d3232eb243467b2ea4e10f05a06de76929c0
svojnovic <37151265+svojnovic@users.noreply.github.com>
2022-09-29 13:59:55 +0200

    Completed Module

--- ssg/site.py
+++ ssg/site.py
@@ -1,4 +1,3 @@
-@@ -1,6 +1,8 @@
 import sys
 from pathlib import Path
 
@@ -10,13 +9,16 @@ class Site:
         self.source = Path(source)
         self.dest = Path(dest)
         self.parsers = parsers or []
+
     def create_dir(self, path):
         directory = self.dest / path.relative_to(self.source)
         directory.mkdir(parents=True, exist_ok=True)
+
     def load_parser(self, ext):
         for parser in self.parsers:
             if parser.valid_file_ext(ext):
                 return parser
+
     def run_parser(self, path):
         parser = self.load_parser(path.suffix)
         if parser is not None:
@@ -35,6 +37,7 @@ class Site:
                 self.create_dir(path)
             elif path.is_file():
                 self.run_parser(path)
+
     @staticmethod
     def error(message):
         sys.stderr.write("\x1b[1;31m{}\n".format(message))
